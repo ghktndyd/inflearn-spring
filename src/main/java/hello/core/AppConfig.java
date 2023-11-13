d85b812f3bd6f295bfbd8118f5a9f2140932807f
sooyong <hwasuyong99@naver.com>
2023-11-13 22:54:10 +0900

    @Configuration과 싱글톤

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -13,23 +13,35 @@ import org.springframework.context.annotation.Configuration;
 @Configuration // 설정 정보를 담당하는 클래스에 붙이는 어노테이션
 public class AppConfig {
 
+    // @Bean memberService()이 한 번 호출이 되면
+    // new MemoryMemberRepository()는 같이 호출이 된다. = getMemberRepository()의 구현부
+
+    // @Bean orderService()도 한 번 호출이 되면
+    // new OrderServiceImpl()이 호출이 되는데 이때 new MemoryMemberRepository()는 같이 호출이 된다.
+
+    // new 키워드로 계속 객체를 생성이 되는데 싱글톤이 깨지는 게 아닌가?
+
+
     @Bean // 스프링 컨테이너가 관리하는 객체로 설정하는 어노테이션
     public MemberService memberService() {
-        return new MemberServiceImpl(getMemberRepository());
+        System.out.println("call AppConfig.memberService");
+        return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
-    public static MemoryMemberRepository getMemberRepository() {
+    public MemoryMemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
-        return new OrderServiceImpl(getMemberRepository(), getDiscountPolicy());
+        System.out.println("call AppConfig.orderService");
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     @Bean
-    public static DiscountPolicy getDiscountPolicy() {
+    public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
 

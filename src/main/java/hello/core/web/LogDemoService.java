016d95bf435b6509d4dfd60bd75ab164638ec9ae
sooyong <hwasuyong99@naver.com>
2023-11-23 08:22:48 +0900

    request 스코프 예제 만들기

---
+++ src/main/java/hello/core/web/LogDemoService.java
@@ -0,0 +1,20 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class LogDemoService {
+
+    private final MyLogger myLogger;
+
+    public void logic(String id) {
+        myLogger.log("service id = " + id);
+    }
+
+    /**
+     * 웹과 관련이 전혀 없는 정보는 컨트롤러까지만 사용해야 한다. 서비스 계층부터는 웹 기술에 종속되지 않는 게 좋다.
+     */
+}

a2e43595ea7d185b987b1a4377d5e7b080c1970e
ghktndyd <hwasuyong99@naver.com>
2023-12-14 00:17:52 +0900

    상품 등록 처리 - @ModelAttribute

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -4,10 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
@@ -19,38 +16,79 @@ import lombok.RequiredArgsConstructor;
 @Controller
 public class BasicItemController {
 
-	private final ItemRepository itemRepository;
+    private final ItemRepository itemRepository;
 
-	@GetMapping
-	public String items(Model model) {
-		List<Item> items = itemRepository.findAll();
-		model.addAttribute("items", items);
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
 
-		return "basic/items";
-	}
+        return "basic/items";
+    }
 
-	@GetMapping("/{itemId}")
-	public String item(@PathVariable long itemId, Model model) {
-		Item item = itemRepository.findById(itemId);
-		model.addAttribute("item", item);
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
 
-		return "basic/item";
-	}
+        return "basic/item";
+    }
 
-	@GetMapping("/add")
-	public String addForm() {
-		return "basic/addForm";
-	}
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
 
-	@PostMapping("/add")
-	public String save() {
-		return "basic/addForm";
-	}
+    //    @PostMapping("/add")
+    public String addItemV1(@RequestParam String itemName,
+                            @RequestParam int price,
+                            @RequestParam Integer quantity,
+                            Model model) {
 
-	// 테스트용
-	@PostConstruct
-	public void init() {
-		itemRepository.save(new Item("itemA", 10000, 10));
-		itemRepository.save(new Item("itemB", 20000, 20));
-	}
+        Item item = new Item();
+
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+    //   @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item, Model model) {
+        // @ModelAttribute("뷰에 넘기는 속성이름")
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+    //    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item) {
+        // class명의 첫글자를 소문자로 바꾼다. ex) Item -> item
+        itemRepository.save(item);
+
+        return "basic/item";
+    }
+
+    @PostMapping("/add")
+    public String addItemV4(Item item) {
+        // @ModelAttribute도 생략 가능, String, int 같은 타입은 @RequestParam이 생략된 걸로 해석
+        itemRepository.save(item);
+
+        return "basic/item";
+    }
+
+    // 테스트용
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+    }
 }

821d79de1ef0cc1b65c720a07dd5ae4672f7a47f
sooyong <hwasuyong99@naver.com>
2023-12-29 08:34:40 +0900

    엔티티 설계시 주의점

--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,13 +1,17 @@
 package jpabook.jpashop.domain;
 
+import static jakarta.persistence.FetchType.*;
+
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
@@ -27,17 +31,34 @@ public class Order {
 	@Id @GeneratedValue
 	private Long id;
 
-	@ManyToOne @JoinColumn(name = "member_id")
+	@ManyToOne(fetch = LAZY) @JoinColumn(name = "member_id")
 	private Member member;
 
-	@OneToMany(mappedBy = "order")
+	@OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
 	private List<OrderItem> orderItems = new ArrayList<>();
 
-	@OneToOne @JoinColumn(name = "delivery_id")
+	@OneToOne(fetch = LAZY) @JoinColumn(name = "delivery_id")
 	private Delivery delivery;
 
 	private LocalDateTime orderDate; // 주문 시간
 
 	@Enumerated(EnumType.STRING)
 	private OrderStatus status; // 주문 상태 [ORDER, CANCEL]
+
+	//== 연관관계 편의 메서드 ==//
+	public void setMember(Member member) {
+		this.member = member;
+		member.getOrders().add(this);
+	}
+
+	public void addOrderItem(OrderItem orderItem) {
+		orderItems.add(orderItem);
+		orderItem.setOrder(this);
+	}
+
+	public void setDelivery(Delivery delivery) {
+		this.delivery = delivery;
+		delivery.setOrder(this);
+	}
+
 }
--- jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
@@ -20,12 +21,12 @@ public class OrderItem {
 	@Id
 	private Long id;
 
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "item_id")
 	private Item item;
 
 	@JoinColumn(name = "order_id")
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	private Order order; // 하나의 오더가 여러개의 아이템을 가질 수 있다.
 
 	private int orderPrice; // 주문 가격

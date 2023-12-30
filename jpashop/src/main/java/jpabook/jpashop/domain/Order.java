69647988f94a5a2d806dd235b8cc61a366d926d2
sooyong <hwasuyong99@naver.com>
2023-12-31 08:24:44 +0900

    주문, 주문상품 엔티티 개발

--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -22,22 +22,26 @@ import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter @Setter
+@Getter
+@Setter
 @Table(name = "orders")
 @Entity
 public class Order {
 
 	@Column(name = "order_id")
-	@Id @GeneratedValue
+	@Id
+	@GeneratedValue
 	private Long id;
 
-	@ManyToOne(fetch = LAZY) @JoinColumn(name = "member_id")
+	@ManyToOne(fetch = LAZY)
+	@JoinColumn(name = "member_id")
 	private Member member;
 
 	@OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
 	private List<OrderItem> orderItems = new ArrayList<>();
 
-	@OneToOne(fetch = LAZY) @JoinColumn(name = "delivery_id")
+	@OneToOne(fetch = LAZY)
+	@JoinColumn(name = "delivery_id")
 	private Delivery delivery;
 
 	private LocalDateTime orderDate; // 주문 시간
@@ -59,6 +63,46 @@ public class Order {
 	public void setDelivery(Delivery delivery) {
 		this.delivery = delivery;
 		delivery.setOrder(this);
+	}
+
+	//== 생성 메서드 ==//
+	public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+		Order order = new Order();
+		order.setMember(member);
+		order.setDelivery(delivery);
+		for (OrderItem orderItem : orderItems) {
+			order.addOrderItem(orderItem);
+		}
+		order.setStatus(OrderStatus.ORDER);
+		order.setOrderDate(LocalDateTime.now());
+
+		return order;
+	}
+
+	//== 비즈니스 로직 ==//
+	/**
+	 * 주문 취소
+	 */
+	public void cancel() {
+		if (delivery.getDeliveryStatus() == DeliveryStatus.COMP) {
+			throw new IllegalStateException("이미 배송이 완료된 상품입니다.");
+		}
+
+		this.setStatus(OrderStatus.CANCEL);
+
+		for (OrderItem orderItem : orderItems) {
+			orderItem.cancel();
+		}
+	}
+
+	//== 조회 로직 ==//
+	/**
+	 * 전체 주문 가격 조회
+	 */
+	public int getTotalPrice() {
+		return orderItems.stream()
+			.mapToInt(OrderItem::getTotalPrice)
+			.sum();
 	}
 
 }

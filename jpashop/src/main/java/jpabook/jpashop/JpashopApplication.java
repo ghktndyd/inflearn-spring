7cbf82114ece369f2b7fe969e5f9ceff1bcd9ce8
sooyong <hwasuyong99@naver.com>
2023-12-31 22:10:14 +0900

    다음으로

--- jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -3,6 +3,8 @@ package jpabook.jpashop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 다음으로
+
 @SpringBootApplication
 public class JpashopApplication {
 

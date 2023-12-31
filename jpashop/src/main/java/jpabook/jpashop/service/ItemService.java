66df0395fcf397e5a7c52ad2cd404a9f7762ec1b
sooyong <hwasuyong99@naver.com>
2023-12-31 20:45:57 +0900

    변경 감지와 병합(merge)

--- jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -28,4 +28,21 @@ public class ItemService {
 	public Item findOne(Long itemId) {
 		return itemRepository.findOne(itemId);
 	}
+
+	/**
+	 * 변경감지로 업데이트
+	 */
+	@Transactional
+	public void updateItem(Long itemId, String name, int price, int stockQuantity) {
+		Item findItem = itemRepository.findOne(itemId);
+
+		findItem.setName(name);
+		findItem.setPrice(price);
+		findItem.setStockQuantity(stockQuantity);
+
+		// 원래 여기서 save를 호출해야 하지만 Jpa가 변경감지를 해서 자동으로 Update를 해주는가?
+		// flush를 날린다?
+		// flush -> Jpa가 변경이 발생한 애를 찾는다.
+	}
+
 }
